f116e01a93fcf57512b6fdce2ce52824e97328bb
Haquangnguyen30 <haquangnguyen30@gmail.com>
2024-05-14 11:08:13 +0700

    fix trang thai hoa don

--- MVC/views/manage/pages/HoaDonPage.php
+++ MVC/views/manage/pages/HoaDonPage.php
@@ -2,13 +2,6 @@
   <div>
     <h1 class = 'styleText-01' >Quản Lý Hoá Đơn</h1>
   </div>
-  
-  <!-- <div class="search">
-    <input type="text" id="txtFind" style="min-width: 300px;" placeholder="Tìm kiếm theo Mã hoặc tên hoá đơn">
-    <input type="button" id="btnSearch" value="Tìm Kiếm">
-  </div>
-  <input type="submit" onclick="DieuHuong()" value="Thêm">
-  <input type="button" id="btnRefresh" onclick="btnRefresh()" value="Làm Tươi"> -->
 
   <div class="search-wrap">
     <div class="search">
@@ -19,16 +12,11 @@
       </label>
     </div>
     <!-- Nút sang form dữ liệu nhóm quyền  -->
-     <!-- Nút sang form dữ liệu nhóm quyền  -->
     <div class="block-wrap">
       <label class="btn btn_reset" for="btnRefresh">
         <i class='bx bx-reset'></i>
         <input type="button" id="btnRefresh" onclick="btnRefresh()" value="" hidden>
       </label>
-      <!-- <div class="btn btn_add"> 
-        <i class='bx bx-plus'></i>
-        <input type="button" class="" onclick="DieuHuong()" value="Thêm">
-      </div> -->
     </div>
   </div>
 
@@ -61,11 +49,11 @@
 <script>
 function DoiTrangThaiHoaDon(obj) {
     var ma = obj.id;
-    var trangThai = obj.value; // Giá trị trạng thái của radio button
+    var trangThai = obj.value; // Giá trị trạng thái của radio button mới được chọn
     var radioButtons = document.querySelectorAll("input[type='radio'][id='" + ma + "']");
     var result;
 
-    // Kiểm tra xem radio button nào được chọn
+    // Kiểm tra xem radio button nào đang được chọn
     var selectedRadioButton = null;
     radioButtons.forEach(function(radioButton) {
         if (radioButton.checked) {
@@ -74,76 +62,62 @@ function DoiTrangThaiHoaDon(obj) {
     });
 
     if (selectedRadioButton === null) {
-        // alert("Vui lòng chọn một trạng thái."); // Thông báo nếu không có radio button nào được chọn
         swal({
-          
-          text: "Vui lòng chọn một trạng thái!",
-        
-        })     
+            text: "Vui lòng chọn một trạng thái!",
+        });
         return;
     }
 
-    // result = confirm("Bạn có muốn đổi trạng thái?");
+    // Kiểm tra nếu radioButton đang được chọn có value = 2
+    if ((selectedRadioButton.value == 2 || selectedRadioButton.value == -1) && trangThai != selectedRadioButton.value) {
+        swal({
+            text: "Không thể chuyển đổi trạng thái hiện tại!",
+            icon: "error",
+        }).then(function(){
+            // Khôi phục trạng thái ban đầu của radio button
+            obj.checked = false;
+            selectedRadioButton.checked = true;
+        });
+        return;
+    }
 
-    // if (result == true) {
-    //     // Gửi yêu cầu AJAX với trạng thái tương ứng
-    //     $.post("http://localhost/WebBanHangMoHinhMVC/AjaxHoaDon/DoiTrangThai", {
-    //         ma: ma,
-    //         trangThai: trangThai
-    //     }, function(data) {
-    //         // alert(data); // Hiển thị thông báo
-    //         location.reload();
-    //     });
-
-    //     // Đảm bảo chỉ một radio button được chọn
-    //     radioButtons.forEach(function(radioButton) {
-    //         if (radioButton != selectedRadioButton) {
-    //             radioButton.checked = false;
-    //         }
-    //     });
-    // } else {
-    //     obj.checked = !obj.checked; // Đảo ngược trạng thái của radio button nếu người dùng không chấp nhận thay đổi
-    // }
     swal({
-      title: "Thông báo",
-      text: "Bạn có muốn đổi trạng thái?!",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
+        title: "Thông báo",
+        text: "Bạn có muốn đổi trạng thái?!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
     })
-    .then((willDelete) => {
-        if (willDelete) {
+    .then((willChange) => {
+        if (willChange) {
             $.post("http://localhost/WebBanHangMoHinhMVC/AjaxHoaDon/DoiTrangThai", {
                 ma: ma,
                 trangThai: trangThai
             }, function(data) {
-                // alert(data); // Hiển thị thông báo
                 swal({
-                  title: data,
-                  text: "Nhấn để tiếp tục!",
-                  icon: "success",              
+                    title: data,
+                    text: "Nhấn để tiếp tục!",
+                    icon: "success",
                 }).then(function(){
-                  // location.reload();
-                  loadTable("", index, size)
-                  loadPhanTrang("hoadon", index, size, sql, "");
-                })
-               
+                    // location.reload();
+                    loadTable("", index, size);
+                    loadPhanTrang("hoadon", index, size, sql, "");
+                });
             });
-            
-        // Đảm bảo chỉ một radio button được chọn
-          radioButtons.forEach(function(radioButton) {
-              if (radioButton != selectedRadioButton) {
-                  radioButton.checked = false;
-              }
-          });
+
+            // Đảm bảo chỉ một radio button được chọn
+            radioButtons.forEach(function(radioButton) {
+                if (radioButton != selectedRadioButton) {
+                    radioButton.checked = false;
+                }
+            });
         } else {
-            obj.checked = !obj.checked; // Đảo ngược trạng thái của radio button nếu người dùng không chấp nhận thay đổi
+            obj.checked = false; // Đảo ngược trạng thái của radio button nếu người dùng không chấp nhận thay đổi
+            selectedRadioButton.checked = true;
             swal("Bạn đã hủy đổi trạng thái!");
         }
     });
 }
-
-
 
 
 
